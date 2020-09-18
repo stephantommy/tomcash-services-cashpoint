1c2847800c97a4c811285754e072cdadc3324114
stephantommy <steptomytomb@gmail.com>
2020-09-19 01:49:52 +0700

    update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,5 @@ module.exports = {
     port: process.env.PORT || 3001,
 
     mongoUrl: process.env.MONGODB_URL || 'mongodb+srv://db_user:db_pass@cluster0.5vgix.mongodb.net/<testDB>?retryWrites=true&w=majority',
-    //mongoUrl: 'mongodb://localhost:27017/testDB'
+    //mongoUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/testDB'
 }
